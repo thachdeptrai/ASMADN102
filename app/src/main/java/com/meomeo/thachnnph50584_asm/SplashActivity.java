98eb3ee60f5a86be11c9da9f417bb4648ab826ab
NgocThach <ngocthachgenz@gmail.com>
2025-02-11 12:26:35 +0700

    full asm2

---
+++ app/src/main/java/com/meomeo/thachnnph50584_asm/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.meomeo.thachnnph50584_asm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
